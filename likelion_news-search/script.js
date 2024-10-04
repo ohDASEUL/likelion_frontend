605284b5c30d73c47930a8fed08fd41f60eeeedb
ohDASEUL <comeseul312@gmail.com>
2024-10-05 01:05:49 +0900

    멋쟁이사자처럼 자바스크립트 2차 과제 - XMLHttpRequest를 사용하여 뉴스 검색 기능 리팩토링

--- likelion_news-search/script.js
+++ likelion_news-search/script.js
@@ -1,7 +1,10 @@
-function newsSearch() {
-  // 검색어 입력 필드를 가져오기
-  const searchInput = document.querySelector('input[type="text"]');
+"use strict";
 
+// 버튼 요소와 입력 필드 가져오기
+const searchButton = document.querySelector("button");
+const searchInput = document.querySelector('input[type="text"]');
+
+searchButton.addEventListener("click", function () {
   // 검색어가 비어있는지 확인
   if (!searchInput.value.trim()) {
     alert("검색어를 입력하세요.");
@@ -10,8 +13,19 @@ function newsSearch() {
     const newsApiKey = "f692390650b44a1f9846e90dd406fada";
     let newsUrl = `https://newsapi.org/v2/everything?q=${newsTopic}&apiKey=${newsApiKey}`;
 
-    fetch(newsUrl).then(function (response) {
-      console.log(response.json());
-    });
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", newsUrl);
+
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const result = xhr.responseText;
+        const resultObj = JSON.parse(result);
+        console.log(resultObj);
+      } else {
+        console.error("뉴스 데이터를 가져오지 못했습니다.");
+      }
+    };
+
+    xhr.send();
   }
-}
+});
