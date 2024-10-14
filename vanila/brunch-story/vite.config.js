290f5c954714f6e6d8878e09df1f324123a08288
ohDASEUL <comeseul312@gmail.com>
2024-10-15 01:44:24 +0900

    [vanilla - brunch-story 연습] : vite로 프로젝트 생성 후 폴더 구조화 및 assets에 icon 추가

---
+++ vanila/brunch-story/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: './public',  // Vite 서버가 참조할 루트 디렉토리를 지정
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, './public/index.html'), // 기본 index.html
+      },
+    },
+  },
+});
