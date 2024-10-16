7c31f44892cc39b559068214fda29606e2dcaa6c
ohDASEUL <comeseul312@gmail.com>
2024-10-16 15:37:50 +0900

    [vanilla - brunch-story 연습]: develop 브랜치 삭제.. 및 일부 수정

--- vanila/brunch-story/vite.config.js
+++ vanila/brunch-story/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: './',  // Vite 서버가 참조할 루트 디렉토리를 현재 프로젝트 루트로 설정
+  root: './', // Vite 서버가 참조할 루트 디렉토리를 현재 프로젝트 루트로 설정
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, './index.html'),  // index.html 경로를 루트로 설정
+        index: resolve(__dirname, './index.html'), // index.html 경로를 루트로 설정
       },
     },
   },
