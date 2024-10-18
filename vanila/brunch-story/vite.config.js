42b7dd7482d5976086b6119867530a044431f118
ohDASEUL <comeseul312@gmail.com>
2024-10-18 09:00:33 +0900

    [vanilla - brunch-story 연습]: home.html footer 주석 추가 및 css 초기화, 색깔변수 설정, wrapper 사이즈 설정

--- vanila/brunch-story/vite.config.js
+++ vanila/brunch-story/vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: './', // Vite 서버가 참조할 루트 디렉토리를 현재 프로젝트 루트로 설정
+  root: './', // 프로젝트 루트를 Vite의 루트로 설정
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, './index.html'), // index.html 경로를 루트로 설정
+        index: resolve(__dirname, './index.html'),
+        homepage: resolve(__dirname, './src/features/home/homePage.html'), // 필요한 HTML 파일 추가
       },
+    },
+  },
+  server: {
+    fs: {
+      allow: [
+        '.', // 현재 디렉토리 접근 허용
+        './src/assets', // assets 폴더 접근 허용
+      ],
     },
   },
 });
