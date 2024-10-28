b41623d939fbcdcc638c9788efb38cba30c3c687
ohDASEUL <comeseul312@gmail.com>
2024-10-28 21:39:16 +0900

    폴더 📁 수정

---
+++ vanila/brunch-story/temp-directory/server/config/index.js
@@ -0,0 +1,54 @@
+import logger from '#utils/logger.js';
+import dotenv from 'dotenv';
+
+// 기본 .env 파일 로딩(package.json에서 로딩함)
+dotenv.config({ path: '.env' });
+// 환경별 .env 파일 로딩
+logger.log('NODE_ENV', process.env.NODE_ENV);
+if (process.env.NODE_ENV) {
+  dotenv.config({ override: true, path: `.env.${process.env.NODE_ENV}` });
+}
+
+export const db = {
+  url: process.env.DB_URL,
+  clientIds: ['00-sample', '00-brunch', '00-nike', '00-toyzrus', '00-openmarket', 'vanilla00', 'vanilla01', 'vanilla02', 'vanilla03', 'vanilla04', 'vanilla05', 'vanilla06', 'vanilla07', 'vanilla08', 'vanilla09', 'vanilla10'],
+};
+
+export const jwt = {
+  access: {
+    secretKey: 'ShoppingAccessToken', // 암호키
+    options: {
+      algorithm: 'HS256', // 대칭키 방식
+      expiresIn: '1d',  // 하루
+      // expiresIn: '2h',  // 2시간
+      // expiresIn: '10m', // 10분
+      // expiresIn: '10s',  // 10초
+      issuer: 'FESP', // 발행자
+    },
+  },
+  refresh: {
+    secretKey: 'ShoppingRefreshToken',
+    options: {
+      algorithm: 'HS256',
+      expiresIn: '30d',
+      // expiresIn: '30s',
+      issuer: 'FESP',
+    },
+  }
+};
+
+export const cors = {
+  origin: [
+    /^https?:\/\/localhost/,
+    /^https?:\/\/127.0.0.1/,
+    /netlify.app$/,
+    /vercel.app$/,
+    /fesp.shop$/,
+    /codepen.io$/,
+    /stackblitz.com$/,
+    /webcontainer.io$/,
+    new RegExp(process.env.APP_HOST)
+  ]
+};
+
+export default { db, jwt, cors };
