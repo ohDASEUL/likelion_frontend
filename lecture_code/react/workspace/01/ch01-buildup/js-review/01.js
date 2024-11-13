f7b5422f78adc362feaddc6e3fc3751e727a35c0
ohDASEUL <comeseul312@gmail.com>
2024-11-13 10:42:08 +0900

    [리액트 실습] - js-review 01.jsfor문

--- lecture_code/react/workspace/01/ch01-buildup/js-review/01.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/01.js
@@ -3,5 +3,16 @@ var arr = [10, 20, 30];
 // 각 요소의 제곱값으로 구성된 새로운 배열 생성
 var arr2 = [100, 400, 900];
 
+// for
+var arr2 = [];
+for (let i = 0; i < arr.length; i++) {
+  arr2.push(arr[i] * arr[i]);
+}
+// for of, (ES6)
 
-console.log(arr2);  // [100, 400, 900]
+// forEach, (ES5)
+
+// map(), (ES6)
+
+// Arrow function, (ES6)
+console.log(arr2); // [100, 400, 900]
