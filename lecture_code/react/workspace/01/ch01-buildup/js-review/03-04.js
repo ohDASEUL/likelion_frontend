e117651c2f83c28052e5b73b10bda97bc98ae209
ohDASEUL <comeseul312@gmail.com>
2024-11-13 13:39:12 +0900

    [리액트 실습] - js-review 03.js 특정 객체만 깊은 복사하여 불변성 유지하기

--- lecture_code/react/workspace/01/ch01-buildup/js-review/03-04.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/03-04.js
@@ -10,6 +10,9 @@ var newItemList = itemList;
 // 2. 전개 연산자를 이용한 복사(얕은복사)
 var newItemList = [...itemList];
 
+// 3. 객체를 속성으로 가질 경우 깊은 복사를 위해서는 속성도 새로운 객체로 복사해야함(불변성을 지키기 위해서)
+newItemList[1] = { ...itemList[1] };
+
 // itemList, newItemList 비교
 newItemList[1].done = true;
 console.log(itemList, newItemList);
