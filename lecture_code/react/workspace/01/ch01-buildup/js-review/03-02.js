d58d2cd78853634cc1b7412a0fc503842bc1f958
ohDASEUL <comeseul312@gmail.com>
2024-11-13 11:47:56 +0900

    [리액트 실습] - js-review 03.js객체 얕은 복사

--- lecture_code/react/workspace/01/ch01-buildup/js-review/03-02.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/03-02.js
@@ -1,2 +1,8 @@
-var item = { no: 1, todo: '두부', done: true };
+var item = { no: 1, todo: "두부", done: true };
 
+var newItem = item;
+
+// item, newItem 비교
+newItem.done = false;
+console.log(item, newItem);
+console.log("같은 객체인가?", item === newItem);
