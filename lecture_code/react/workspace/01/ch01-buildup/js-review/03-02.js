1146843f25a8bea292ae443425cf648ebea1df3a
ohDASEUL <comeseul312@gmail.com>
2024-11-13 13:10:11 +0900

    [리액트 실습] - js-review 03.jsObject.assign을 사용한 객체 복사

--- lecture_code/react/workspace/01/ch01-buildup/js-review/03-02.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/03-02.js
@@ -1,6 +1,12 @@
 var item = { no: 1, todo: "두부", done: true };
 
+// 1. 대입 연산자로 newItem 생성
 var newItem = item;
+
+// 2. Object.assign() 사용해서 속성 추가
+// Object.assign(target, ...source) : target 객체에 source 객체들의 속성을 추가해서 target을 반환
+var newItem = Object.assign(item, { delete: true }, {}, {});
+var newItem = Object.assign({}, item, { delete: true });
 
 // item, newItem 비교
 newItem.done = false;
