7f3c41c471fc92e0fd9a74cfdc93e0823cb41786
ohDASEUL <comeseul312@gmail.com>
2024-11-13 14:49:16 +0900

    [리액트 실습] - js-review 06.js 동기 함수 호출

--- lecture_code/react/workspace/01/ch01-buildup/js-review/06-01.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/06-01.js
@@ -1,23 +1,24 @@
-function f1(){
+function f1() {
   console.log(`f1 작업 시작.`);
   console.log(`f1 작업중...`);
   // ......
   console.log(`f1 작업 종료.`);
-  return (`f1의 결과물`);
+  return `f1의 결과물`;
 }
 
-function f2(f1Result){
+function f2(f1Result) {
   console.log(`${f1Result}로 f2 작업 시작.`);
   console.log(`f2 작업중...`);
   // ......
   console.log(`f2 작업 종료.`);
-  return (`최종 결과물`);
+  return `최종 결과물`;
 }
 
-function test(){
-  
+function test() {
+  f1();
+  f2();
 }
 
-console.log('1. 테스트 시작.');
+console.log("1. 테스트 시작.");
 test();
-console.log('테스트 완료.');
+console.log("테스트 완료.");
