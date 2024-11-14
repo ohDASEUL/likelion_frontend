8e2ac274012f65a8829e734261433d157b83ce06
ohDASEUL <comeseul312@gmail.com>
2024-11-14 11:08:41 +0900

    [리액트 실습] - js-review 08.js 메모이제이션1

--- lecture_code/react/workspace/01/ch01-buildup/js-review/08.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/08.js
@@ -1,14 +1,20 @@
 // 지정한 수가 소수인지 여부를 반환
-var isPrime = function(num){
-  console.time('소요 시간');
-  console.log('소수 판별 시작.', num);
+var isPrime = function (num) {
+  console.time("소요 시간");
+  console.log("소수 판별 시작.", num);
 
   // TODO: 소수 판별 코드
-  let prime = true;
+  let prime = num > 1; // 1은 소수가 아님
 
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      prime = false;
+      break;
+    }
+  }
 
-  console.log('소수 판별 결과.', prime);
-  console.timeEnd('소요 시간');
+  console.log("소수 판별 결과.", prime);
+  console.timeEnd("소요 시간");
   return prime;
 };
 
@@ -21,6 +27,6 @@ isPrime(6);
 isPrime(7);
 isPrime(8);
 isPrime(9);
-isPrime(1000000007);
+isPrime(1000000007); // 십억칠
 isPrime(1000000007);
 isPrime(1000000007);
