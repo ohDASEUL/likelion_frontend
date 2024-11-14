c76a8e3aa14e68f6b066632b49243ec7f2858453
ohDASEUL <comeseul312@gmail.com>
2024-11-14 11:29:11 +0900

    [리액트 실습] - js-review 08.js 메모이제이션3

--- lecture_code/react/workspace/01/ch01-buildup/js-review/08.js
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/08.js
@@ -1,5 +1,5 @@
 // 지정한 수가 소수인지 여부를 반환
-var isPrime2 = function (num) {
+var isPrime = function (num) {
   console.time("소요 시간");
   console.log("소수 판별 시작.", num);
 
@@ -18,19 +18,33 @@ var isPrime2 = function (num) {
   return prime;
 };
 
-var isPrime = function (num) {
-  // 캐시를 위한 코드
-  isPrime._cache = isPrime._cache || {};
-  if (isPrime._cache[num] !== undefined) {
-    // 캐시 되어있음(cache hit)
-    console.log("cache hit", num, isPrime._cache[num]);
-    return isPrime._cache[num];
-  } else {
-    // 소수 판별 코드
-    return (isPrime._cache[num] = isPrime2(num));
-  }
-};
+// var isPrime = function (num) {
+//   // 캐시를 위한 코드
+//   isPrime._cache = isPrime._cache || {};
+//   if (isPrime._cache[num] !== undefined) {
+//     // 캐시 되어있음(cache hit)
+//     console.log("cache hit", num, isPrime._cache[num]);
+//     return isPrime._cache[num];
+//   } else {
+//     // 소수 판별 코드
+//     return (isPrime._cache[num] = isPrime2(num));
+//   }
+// };
 
+// 지정한 변수에 메모이제이션 기능 추가
+function memo(fn) {
+  return function (args) {
+    fn._cache = fn._cache || {};
+    if (fn._cache[args] !== undefined) {
+      console.log("cache hit", args, fn._cache[args]);
+      return fn._cache[args];
+    } else {
+      return (fn._cache[args] = fn(args));
+    }
+  };
+}
+
+var isPrime = memo(isPrime); // 메모이제이션 기능 추가
 isPrime(1);
 isPrime(2);
 isPrime(3);
