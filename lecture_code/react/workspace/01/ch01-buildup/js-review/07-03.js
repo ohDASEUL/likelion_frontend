4c1b7689a4a40a1e59e6b40807899fade64b09e4
ohDASEUL <comeseul312@gmail.com>
2024-11-14 10:21:50 +0900

    [리액트 실습] - js-review 07.js find

---
+++ lecture_code/react/workspace/01/ch01-buildup/js-review/07-03.js
@@ -0,0 +1,15 @@
+var array = [6, 7, 8, 1, 4, 5, 2, 3, 9, 10];
+
+var result = 0;
+
+// - 를 반환할 경우 a, b 정렬
+// + 를 반환할 경우 b, a 정렬
+// = 을 반환할 경우 그대로
+array.sort((a, b) => {
+  a - b;
+});
+console.log(array);
+
+var result = array.find((num) => num % 2 === 0 && num % 3 === 0);
+
+console.log(result);
