e852a471a8f5f39ebba8e6bce9ee4b3b344c2687
ohDASEUL <comeseul312@gmail.com>
2024-11-18 14:00:16 +0900

    [리액트 실습] - 회원가입 입력값 상태관리 - 에러 핸들러? 추가

--- lecture_code/react/workspace/02/ch02-start/state/05/src/App.jsx
+++ lecture_code/react/workspace/02/ch02-start/state/05/src/App.jsx
@@ -1,49 +1,122 @@
 import { useState } from "react";
 
+const errorStyle = {
+  fontSize: "12px",
+  color: "red",
+  fontWeight: "bold",
+};
+
+const emailExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const cellphoneExp = /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/;
+
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cellphone, setCellphone] = useState("010");
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [cellphone, setCellphone] = useState('010');
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    cellphone: "010",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  // const handleNameChange = (event) => {
+  //   setName(event.target.value);
+  // };
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // };
+  // const handleCellphoneChange = (event) => {
+  //   setCellphone(event.target.value);
+  // };
+
+  const handleChange = (event) => {
+    const newUser = {
+      ...user,
+      [event.target.name]: event.target.value,
+    };
+    setUser(newUser);
   };
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+  const onSubmit = (event) => {
+    // 브라우저의 기본 동작 취소(submit 동작 취소)
+    event.preventDefault();
+
+    let newErrors;
+    // 입력 메세지 검증 작업
+    if (user.name.trim() === "") {
+      newErrors = {
+        name: { message: "이름을 입력하세요." },
+      };
+    } else if (user.name.trim().length < 2) {
+      newErrors = {
+        name: { message: "2글자 이상 입력하세요." },
+      };
+    } else if (user.email.trim() === "") {
+      newErrors = {
+        email: { message: "이메일을 입력하세요." },
+      };
+    } else if (user.cellphone.trim() === "") {
+      newErrors = {
+        cellphone: { message: "휴대폰 번호를 입력하세요." },
+      };
+    } else if (emailExp.test(user.email)) {
+      newErrors = {
+        cellphone: { message: "이메일 양식에 맞지 않습니다." },
+      };
+    } else if (cellphoneExp.test(user.email)) {
+      newErrors = {
+        cellphone: { message: "휴대폰 형식에 맞지 않습니다." },
+      };
+    }
+
+    if (newErrors) {
+      setErrors(newErrors);
+    } else {
+      setErrors({});
+    }
   };
 
-  const handleCellphoneChange = (event) => {
-    setCellphone(event.target.value);
-  };
   return (
     <>
       <h1>05 회원가입 입력값 상태 관리</h1>
-      <form>
+
+      <form onSubmit={onSubmit}>
         <label htmlFor="name">이름</label>
-        <input id="name" name="name" value={name} onChange={handleNameChange} />
+        <input
+          id="name"
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+        />
         <br />
-        <div>검증 실패 메세지</div>
+        <div style={errorStyle}>{errors.name?.message}</div>
+
         <label htmlFor="email">이메일</label>
         <input
           id="email"
           name="email"
-          value={email}
-          onChange={handleEmailChange}
+          value={user.email}
+          onChange={handleChange}
         />
         <br />
-        <div>검증 실패 메세지</div>
+        <div style={errorStyle}>{errors.email?.message}</div>
+
         <label htmlFor="cellphone">휴대폰</label>
         <input
           id="cellphone"
           name="cellphone"
-          value={cellphone}
-          onChange={handleCellphoneChange}
+          value={user.cellphone}
+          onChange={handleChange}
         />
         <br />
-        <div>검증 실패 메세지</div>
+        <div style={errorStyle}>{errors.cellphone?.message}</div>
+
         <button type="submit">가입</button>
       </form>
+
       <p>
         이름: <br />
         이메일: <br />
