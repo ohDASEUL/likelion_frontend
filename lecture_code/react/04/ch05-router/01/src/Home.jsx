3d341bfd14c79c68a1ed4fb026e9df603609590c
ohDASEUL <comeseul312@gmail.com>
2024-11-22 11:22:38 +0900

    [리액트 실습] 🛣️ React Router 도입을 위한 초기 컴포넌트 구조 설정 ✨ Features
    SPA(Single Page Application) 구조를 위한 기본 페이지 컴포넌트 구성
    공통 Header 컴포넌트를 통한 네비게이션 구조 설계
    페이지별 컴포넌트 분리 (Home, Page1, Page2)
    
    🏗 Component Structure
    
    App: 최상위 루트 컴포넌트
    Home: 메인 페이지 컴포넌트
    Page1, Page2: 추가 페이지 컴포넌트
    Header: 공통 네비게이션 컴포넌트
    
    📝 Current Limitations
    
    일반 HTML a 태그를 사용하여 페이지 전환 (전체 페이지 리로드 발생)
    .html 확장자를 사용한 전통적인 링크 방식
    SPA의 장점을 활용하지 못하는 구조
    
    🔄 Next Steps
    
    React Router 라이브러리 도입 예정
    a 태그를 Link 컴포넌트로 교체 필요
    라우트 설정을 통한 컴포넌트 전환 구현 예정

---
+++ lecture_code/react/04/ch05-router/01/src/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "@components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <h2>Home</h2>
+    </>
+  );
+}
+export default Home;
---
+++ lecture_code/react/04/ch05-router/01/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>리액트 라우터</h1>
+      <a href="home.html">home</a>
+      <br />
+      <a href="page1.html">page1</a>
+      <br />
+      <a href="page2.html">page2</a>
+    </header>
+  );
+}
+
+export default Header;
