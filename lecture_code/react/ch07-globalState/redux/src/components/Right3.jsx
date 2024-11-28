e5cebe43978633cfc392e4782d90c0d1147d1ee2
ohDASEUL <comeseul312@gmail.com>
2024-11-28 14:34:06 +0900

    [리액트 실습] ♻️ Redux Toolkit 액션 디스패치 방식으로 변경
    ✨ 주요 기능
    - Redux Toolkit의 createSlice로 생성된 액션 크리에이터 적용
    - 기존 카운터 조작 기능 유지 (증가, 감소, 초기화)
    
    🔨 구현 사항
    - 액션 디스패치 방식 변경:
      - 기존 counterActionCreator 임포트 제거
      - createSlice에서 자동 생성된 액션 크리에이터 사용
    - 컴포넌트 로직 단순화:
      - 액션 객체 생성 과정 간소화
      - 불필요한 보일러플레이트 코드 제거
    
    🎯 기술적 세부사항
    - Redux Toolkit 액션 크리에이터 특징:
      - payload 자동 처리
      - 타입 문자열 자동 생성
      - 액션 객체 구조 단순화
    - dispatch 호출 구문 간소화:
      - countUp(2) 형태로 직접 호출 가능
      - payload 객체 자동 구성

--- lecture_code/react/ch07-globalState/redux/src/components/Right3.jsx
+++ lecture_code/react/ch07-globalState/redux/src/components/Right3.jsx
@@ -1,4 +1,5 @@
-import counterActionCreator from "@redux/counterActionCreator";
+import { countDown, countReset, countUp } from "@redux-toolkit/counterSlice"; // redux-toolkit
+// import counterActionCreator from "@redux/counterActionCreator"; // redux
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
@@ -12,15 +13,9 @@ function Right3() {
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>
-        -1
-      </button>
-      <button onClick={() => dispatch(counterActionCreator.countReset())}>
-        0
-      </button>
-      <button onClick={() => dispatch(counterActionCreator.countUp(2))}>
-        +2
-      </button>
+      <button onClick={() => dispatch(countDown(1))}>-1</button>
+      <button onClick={() => dispatch(countReset())}>0</button>
+      <button onClick={() => dispatch(countUp(2))}>+2</button>
     </div>
   );
 }
