a392e4ca9bb8d6bae3cde8b5362a137d92cf69da
ohDASEUL <comeseul312@gmail.com>
2024-12-11 11:17:04 +0900

    [리액트 실습] 🔑 Axios 인스턴스 인증 처리 개선
    ✨ 주요 개선사항
    - Zustand 스토어 연동을 통한 인증 토큰 관리
    - 하드코딩된 토큰 제거
    - client-id 수정 ('brunch' → 'board')
    
    🔍 상세 변경사항
    1. 인증 처리
       - useUserStore를 통한 동적 토큰 관리 구현
       - 하드코딩된 JWT 토큰 제거
       - 조건부 토큰 주입 로직 추가
    
    2. 구성 개선
       - client-id 식별자 수정
       - 불필요한 주석 코드 제거
       - 주석 내용 명확화
    
    🔧 기술적 개선
    - 전역 상태 관리와 연동
    - 인증 토큰 동적 처리
    - 코드 보안성 강화
    
    🛡️ 보안 강화
    - 하드코딩된 인증 정보 제거
    - 중앙화된 인증 토큰 관리
    - 안전한 토큰 처리 구현

--- lecture_code/react/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
+++ lecture_code/react/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
@@ -1,32 +1,27 @@
+import useUserStore from "@zustand/userStore";
 import axios from "axios";
 
 function useAxiosInstance() {
+  const { user } = useUserStore;
+
   const instance = axios.create({
     baseURL: "https://11.fesp.shop",
     timeout: 1000 * 15,
     headers: {
       "Content-Type": "application/json", // request의 데이터 타입
       accept: "application/json", // response의 데이터 타입
-      "client-id": "00-brunch",
+      "client-id": "00-board",
     },
   });
 
   // 요청 인터셉터 추가하기
   instance.interceptors.request.use((config) => {
-    // config.headers[
-    //   "Authorization"
-    // ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjQ2LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLrsKnquIjqsIDsnoXtlbTrhpPqs6DquYzrqLnsnYDrsJTrs7TsnoXri4jri6QiLCJlbWFpbCI6ImJhYm9AYmFiby5jb20iLCJsb2dpblR5cGUiOiJlbWFpbCIsImlhdCI6MTczMzc5NzI2MSwiZXhwIjoxNzMzODgzNjYxLCJpc3MiOiJGRVNQIn0.OnBFxwtirn67XlYtiu3V6-qLK4lLllSFeGwfNw0uxjc`;
-    // config.headers[
-    //   "Authorization"
-    // ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjMzLCJ0eXBlIjoidXNlciIsIm5hbWUiOiLrtpXslrTrubXsgqzri6zrnbwiLCJlbWFpbCI6ImJ1bmdlb2JiYW5nQHNhZG9sbGEuY29tIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzM3OTkwMzYsImV4cCI6MTczMzg4NTQzNiwiaXNzIjoiRkVTUCJ9.s8ey9cS0VzII_dsYiq0eOoiqkgcU_82qO-BKA2mbWkk`;
-    config.headers[
-      "Authorization"
-    ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjg2LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLsmKTri6TsiqwiLCJlbWFpbCI6ImNvbWVzZXVsMzEyQGdtYWlsLmNvbSIsImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzMzODA2OTE4LCJleHAiOjE3MzM4OTMzMTgsImlzcyI6IkZFU1AifQ.37dCZTSAnZG6zJ9bdSs-PbnxgaSUCxm6sxpwAA1XPLg`;
-
-    // 요청이 전달되기 전에 필요한 공통 작업 수행
+    if (user) {
+      config.headers["Authorization"] = `Bearer ${user.accessToken}`;
+    }
     config.params = {
       delay: 500,
-      ...config.params, // 기존 쿼리스트링 복사
+      ...config.params,
     };
     return config;
   });
