8deac8d70803785038a1456403a601a4be6128c0
ohDASEUL <comeseul312@gmail.com>
2024-12-11 10:05:46 +0900

    [리액트 실습] lion-board step 1

--- lecture_code/react/ch11-skeleton/lion-board-step1/src/App.jsx
+++ lecture_code/react/ch11-skeleton/lion-board-step1/src/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import router from "@/routes";
+import router from '@/routes';
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <RouterProvider router={ router } />
   );
 }
 
-export default App;
+export default App
